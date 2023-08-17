e16f198b141208c223b665d26362d0324a6908a5
sherif14 <85257402+sherif14@users.noreply.github.com>
2023-08-17 17:11:33 +0300

    Update WriteInFile.cpp

--- library/WriteInFile.cpp
+++ library/WriteInFile.cpp
@@ -1,1 +1,15 @@
+#include "WriteInFile.h"
+#include <iostream>
+#include <fstream>
+
+void writeHelloWorldToFile(const char* filename) {
+    std::ofstream outputFile(filename);
+    if (outputFile.is_open()) {
+        outputFile << "HelloWorld" << std::endl;
+        outputFile.close();
+        std::cout << "Successfully wrote 'HelloWorld' to " << filename << std::endl;
+    } else {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+    }
+}
 
