5288871cbe2e9131546e113dba3ec13f3543726f
sherif14 <85257402+sherif14@users.noreply.github.com>
2023-08-17 17:13:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,22 @@
 
+#include "WriteInFile.h"
+#include "Print.h"
+#include <iostream>
+#include <string>
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " -c (console) | -f (file)" << std::endl;
+        return 1;
+    }
+
+    if (std::string(argv[1]) == "-c") {
+        printHelloWorld();
+    } else if (std::string(argv[1]) == "-f") {
+        writeHelloWorldToFile("helloWorld.txt");
+    } else {
+        std::cerr << "Invalid argument: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
